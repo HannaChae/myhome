603069cf987006f8969621867b30ead6f4a56d9d
HannaChae <hannachae.dev@gmail.com>
2022-04-08 13:50:40 +0900

    FetchType

--- src/main/java/com/example/myhome/model/User.java
+++ src/main/java/com/example/myhome/model/User.java
@@ -1,5 +1,6 @@
 package com.example.myhome.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -18,6 +19,7 @@ public class User {
     private String password;
     private Boolean enabled;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(
             name = "user_role",
@@ -25,6 +27,8 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    @JsonIgnore
     private List<Board> boards = new ArrayList<>();
 }
